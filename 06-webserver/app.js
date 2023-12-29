eb3e79a16b93ca4cd6175a18c3f41d72747bd6c6
Alan <alan.melgar.f@gmail.com>
2023-12-29 14:56:48 -0400

    Server contenido

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -2,19 +2,19 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/',  (req, res) => {
-  res.send('Homa page')
-})
+// Servir contenido estatico
+app.use(express.static('public'))
+
 
 app.get('/hola-mundo',  (req, res) => {
     res.send('hola mundo en su respectiva ruta')
 })
 
 app.get('*',  (req, res) =>{
-    res.send('404 | page not found')
+    res.sendFile(__dirname + '/public/404.html');
 })
   
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+  }) 
